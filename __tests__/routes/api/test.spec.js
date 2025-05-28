4645d1047917c1369cadd54af0e4509be616f638
M Agung Dirgantara Azly Bagus Wibawa <agungmasda29@gmail.com>
2025-05-29 00:35:31 +0700

    test(api): add tests for access token validation and image resizing
    - Added tests for access token validation, including invalid token, token type, user ID in payload, and revoked token scenarios.
    - Added a test to check the image resizing functionality with valid width and height parameters.
    - Enhanced authentication tests by checking responses for both valid and invalid access tokens.

--- __tests__/routes/api/test.spec.js
+++ __tests__/routes/api/test.spec.js
@@ -1,8 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 const request = require('supertest');
+const { jwtHelper } = require('@exzly-helpers');
 const app = require('@exzly-routes');
 const { createRoute } = require('@exzly-utils');
+
+const usersToken = {
+  adminAccessToken: '',
+  adminRefreshToken: '',
+  memberAccessToken: '',
+  memberRefreshToken: '',
+};
+
+beforeAll(async () => {
+  const adminSignIn = await request(app)
+    .post(createRoute('api', '/auth/sign-in'))
+    .send({ identity: 'admin', password: 'admin' })
+    .expect(200);
+
+  // Set authorization token
+  usersToken.adminAccessToken = adminSignIn.body.accessToken;
+  usersToken.adminRefreshToken = adminSignIn.body.refreshToken;
+
+  const memberSignIn = await request(app)
+    .post(createRoute('api', '/auth/sign-in'))
+    .send({ identity: 'member', password: 'member' })
+    .expect(200);
+
+  // Set authorization token
+  usersToken.memberAccessToken = memberSignIn.body.accessToken;
+  usersToken.memberRefreshToken = memberSignIn.body.refreshToken;
+});
 
 describe('RESTful-API: Test', () => {
   let uploadedDiskStorage;
@@ -54,6 +82,58 @@ describe('RESTful-API: Test', () => {
           .get(createRoute('web', `/storage/${uploadedDiskStorage}`))
           .expect(200);
       });
+
+      it('test 3: should return 200 and resize image when valid width and height parameters are provided', async () => {
+        await request(app)
+          .get(createRoute('web', `/storage/${uploadedDiskStorage}`))
+          .query({ height: 200, width: 200 })
+          .expect(200);
+      });
+    });
+  });
+
+  describe('Access token', () => {
+    it('test 1: should return 401 when an invalid access token is provided', async () => {
+      await request(app)
+        .get(createRoute('api', '/test/access-token'))
+        .set('Authorization', 'unknown token')
+        .expect(401);
+    });
+
+    it('test 2: should return 401 when an invalid token type is provided', async () => {
+      await request(app)
+        .get(createRoute('api', '/test/access-token'))
+        .set('Authorization', `Bearer ${usersToken.adminRefreshToken}`)
+        .expect(401);
+    });
+
+    it('test 3: should return 401 when no user id is present in the payload', async () => {
+      await request(app)
+        .get(createRoute('api', '/test/access-token'))
+        .set('Authorization', `Bearer ${jwtHelper.createPasswordResetToken(1337)}`)
+        .expect(401);
+    });
+
+    it('test 4: should return 401 when valid token does not exist in the database', async () => {
+      await request(app)
+        .get(createRoute('api', '/test/access-token'))
+        .set('Authorization', `Bearer ${jwtHelper.createUserToken('access-token', 1)}`)
+        .expect(401);
+    });
+
+    it('test 5: should return 200 when sign out request is made to revoke the token', async () => {
+      await request(app)
+        .post(createRoute('api', '/auth/sign-out'))
+        .send({ refreshToken: usersToken.memberRefreshToken })
+        .set('Authorization', `Bearer ${usersToken.memberAccessToken}`)
+        .expect(200);
+    });
+
+    it('test 6: should return 401 when using a revoked token', async () => {
+      await request(app)
+        .get(createRoute('api', '/test/access-token'))
+        .set('Authorization', `Bearer ${usersToken.memberAccessToken}`)
+        .expect(401);
     });
   });
 });
